c2290b3b5159090674524164722989cdbbd6a865
Jason Vanstone <jason@vanstoneonline.com>
2021-05-06 02:55:34 -0400

    cleaned footer of extra

--- wordpress/wp-content/themes/corus-theme/footer.php
+++ wordpress/wp-content/themes/corus-theme/footer.php
@@ -19,26 +19,6 @@
 	<?php //get_template_part( 'template-parts/footer/footer-widgets' ); ?>
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-
-		<?php if ( has_nav_menu( 'footer' ) ) : ?>
-			<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'corusone' ); ?>" class="footer-navigation">
-				<ul class="footer-navigation-wrapper">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'items_wrap'     => '%3$s',
-							'container'      => false,
-							'depth'          => 1,
-							'link_before'    => '<span>',
-							'link_after'     => '</span>',
-							'fallback_cb'    => false,
-						)
-					);
-					?>
-				</ul><!-- .footer-navigation-wrapper -->
-			</nav><!-- .footer-navigation -->
-		<?php endif; ?>
 		<div class="site-info">
 			<div class="site-name">
 				<?php if ( has_custom_logo() ) : ?>
@@ -58,13 +38,7 @@
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
-<script type="text/javascript">
-$(document).ready(function(){
-    $(".single-item").slick({
-        dots: true
-    });
-});
-</script>
+
 <?php wp_footer(); ?>
 
 </body>
