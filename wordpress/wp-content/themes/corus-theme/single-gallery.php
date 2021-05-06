5eea3bc6d689da18dbb955893d0320bab07cb9d4
Jason Vanstone <jason@vanstoneonline.com>
2021-05-06 02:57:06 -0400

    made single-gallery dynamic

--- wordpress/wp-content/themes/corus-theme/single-gallery.php
+++ wordpress/wp-content/themes/corus-theme/single-gallery.php
@@ -6,28 +6,46 @@
 get_header();
 
 
-$args = array( 'post_type' => 'gallery', 'posts_per_page' => 10 );
+$args = array( 'post_type' => 'gallery', 'posts_per_page' => 10 );  
 $the_query = new WP_Query( $args ); 
+$post_meta = get_post_custom($post->ID);
+/* foreach ( $post_meta as $key => $value )
+if ( $key ==  'image_1' ||  $key ==  'image_2' || $key ==  'image_3' ) { 
+    echo $key . "=>" . $value . "<br />";
+    foreach ( $value as $imgDiv => $imgValue ) {
+        echo $imgDiv. "=>" . $imgValue . "<br />";
+    }
+}
+ */
+
 ?>
 
 <?php if ( $the_query->have_posts() ) : ?>
 <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
 <h2><?php the_title(); ?></h2>
 <div class="entry-content">
     <div class="container">
         <div class="single-item" >
             <?php 
-            $image = get_field('field_6092d6213625e');
-            $image2 = get_field('field_6092d69336260');
-            $image3 = get_field('field_6092d69536261');
+           
+            $fields = get_fields( $post->ID, false );
+        
+            if( $fields ): ?>
+    
+                    <?php foreach( $fields as $name => $value ): ?>
 
-            ?>
-            <div> <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" /></div>
-            <div> <img src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" /></div>
-            <div> <img src="<?php echo esc_url($image3['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" /></div>
+                    <?php 
+                    $image = get_field($name);
+                    if( !empty( $image ) ): ?>
+                      <div> <?php echo wp_get_attachment_image( $image, $size );?> </div>
+                    <?php endif; 
+                    ?>
+                    <?php endforeach; ?>
+        
+            <?php endif; ?>
         </div>
     </div>
-
 </div>
 <?php endwhile;
 wp_reset_postdata(); ?>
